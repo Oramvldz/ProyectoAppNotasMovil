24f88dc650f403e041cb9145ac19e782da99b68f
Jose Raul <jr.rodriguez19@info.uas.edu.mx>
2023-12-13 15:41:54 -0700

    Ver Nota Agregado

---
+++ app/src/main/java/com/example/proyectoappnotasmovil/VerNotaActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectoappnotasmovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerNotaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_nota);
+    }
+}
